df178a7fb8b446c2ec3de65c66863e5a57055c12
Gloire Chabu <cortesgloire@gmail.com>
2022-11-29 14:05:42 -0800

    wrote the 3rd function

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
-print("{}".format("".join([chr(x) for x in range(97, 123) if chr(x) != 'e' and chr(x) != 'q'])), end='')
+print("{}".format("".join([chr(x) for x in range(97, 123) \
+if chr(x) != 'e' and chr(x) != 'q'])), end='')
