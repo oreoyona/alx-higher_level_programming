2f32647d0509e1b67689297fd7d64ecd3230fa89
Gloire Chabu <cortesgloire@gmail.com>
2022-11-29 14:30:18 -0800

    hot fix

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def al():
-      return [chr(x) for x in range(97, 123) if chr(x) != 'e' and chr(x) != 'q']
+    return \
+        [chr(x) for x in range(97, 123) if chr(x) != 'e' and chr(x) != 'q']
+
+
 print("{}".format("".join(al())), end='')
