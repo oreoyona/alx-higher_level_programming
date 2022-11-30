2727cf31dd8a46451dd96cb86ccf96022d384b39
Gloire Chabu <cortesgloire@gmail.com>
2022-11-29 16:06:56 -0800

    hot fix

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
-    al = [chr(x) for x in range(97, 123)]
-    if c in al:
-        return True
-    return False
+    if isinstance(c, str):
+        al = [chr(x) for x in range(97, 123)]
+              if c in al:
+                  return True
+              return False
