b8091aaeedf075cf165b4e3294e91f5e7b2f53f0
Gloire Chabu <cortesgloire@gmail.com>
2022-12-02 14:24:03 -0800

    created function 1

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-from add_0 import add
-
+f = __import__('add_0', globals(), locals(), [], 0)
 a = 1;
 b = 2
 
-print("<a {}> + <b {}> = <add({}, {}) {}>".format(a, b, a, b, add(1, 2)))
+print("<a {}> + <b {}> = <add({}, {}) {}>".format(a, b, a, b, f.add(1, 2)))
