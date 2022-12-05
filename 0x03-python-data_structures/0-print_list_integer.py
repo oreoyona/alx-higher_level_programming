c939e33710c7c2f1b022da80b0992ebca4d5ed48
Gloire Chabu <cortesgloire@gmail.com>
2022-12-05 13:21:24 -0800

    hot fix

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    if __name__ == '__main__':
-        if len(my_list) == 0:
-            return
-        for x in my_list:
-            print("{}".format(x))
+    if len(my_list) == 0:
+        return
+    for x in my_list:
+        print("{}".format(x))
     return
