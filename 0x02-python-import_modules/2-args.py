d68729478d522a4904c12e1824614c01da7920dd
Gloire Chabu <cortesgloire@gmail.com>
2022-12-08 04:28:12 -0800

    hot fix

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+if len(sys.argv) == 1:
+    print("0 arguments")
+else:
+    y = 1
+    print("{} arguments:".format(len(sys.argv)))
+    for x in range(1, len(sys.argv)):
+            print("{}: {}".format(x, sys.argv[y]))
+            y = y + 1
