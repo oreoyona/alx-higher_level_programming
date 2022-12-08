4c971fa0806fb4a23333cbaed8386a98e5f75be7
Gloire Chabu <cortesgloire@gmail.com>
2022-12-08 05:51:32 -0800

    hot fix

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
-    names = __import__('hidden_4.pyc')
-    for x in names:
-        if x.startswith('__'):
-            continue
-        print("{}".format(x))
+    import hidden_4
