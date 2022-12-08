8ca42b0f8fbcd2c9ffb0d125defcb8ecf6324335
Gloire Chabu <cortesgloire@gmail.com>
2022-12-08 04:44:51 -0800

    hot fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,11 +2,11 @@
 if __name__ == '__main__':
     import sys
     if len(sys.argv) == 1:
-        print("0 arguments")
+        print("0 arguments.")
     else:
         y = 1
         if len(sys.argv) == 2:
-            print("1 argument")
+            print("1 argument:")
         else:
             print("{} arguments:".format(len(sys.argv) - 1))
         for x in range(1, len(sys.argv)):
