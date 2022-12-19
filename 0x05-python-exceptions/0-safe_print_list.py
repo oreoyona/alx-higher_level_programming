0bda6cd67205444e728ba97634850d4640028c20
Gloire Chabu <cortesgloire@gmail.com>
2022-12-19 05:13:57 -0800

    hot fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
-        print([i for i in range(0, x)])
+        for i in range(0, x):
+            print(my_list[i], end='')
     except:
         return
     
