022ed6842c45d633aa068505883215bd680e9906
Gloire Chabu <cortesgloire@gmail.com>
2022-12-19 06:46:27 -0800

    hot fix

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
         for i in range(0, x):
             if type(my_list[i]) == "int":
                 count = count + 1
-                print("{:d}".format(my_list[i])
+                print("{:d}".format(my_list[i], end='')
         print()
     except Exception as e:
         print(e)
