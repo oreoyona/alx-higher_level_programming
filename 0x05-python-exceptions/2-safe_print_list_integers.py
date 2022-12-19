8b0559c8e7168f1423f7f25db08450018d04cb77
Gloire Chabu <cortesgloire@gmail.com>
2022-12-19 06:44:38 -0800

    hot fix

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    try:
+        for i in range(0, x):
+            if type(my_list[i]) == "int":
+                count = count + 1
+                print("{:d}".format(my_list[i])
+        print()
+    except Exception as e:
+        print(e)
+    return count
